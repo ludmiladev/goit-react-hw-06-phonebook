e4dbbeaba3f31053911d4aa085824d33e05d17af
ludmiladev <45207186+ludmiladev@users.noreply.github.com>
2020-12-14 05:47:54 +0200

    Updates

---
+++ precache-manifest.e3f52b80d3a9787c179ca4dfcd4d7d1d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e6597a911efbc19e566a637d62f47da",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d0a0f53e19a2927c62e9",
+    "url": "/static/css/main.51fe5f5f.chunk.css"
+  },
+  {
+    "revision": "e200658b5acde35e2b52",
+    "url": "/static/js/2.a9b356ba.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.a9b356ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0a0f53e19a2927c62e9",
+    "url": "/static/js/main.666d5713.chunk.js"
+  },
+  {
+    "revision": "bb66ff246ec7d4061b8b",
+    "url": "/static/js/runtime-main.18573a1e.js"
+  }
+]);
